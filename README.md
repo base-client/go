8f85eb07d07bdf09483b5c2876ef9a2a660217a1
heaven-chp <62167253+heaven-chp@users.noreply.github.com>
2020-09-03 08:04:17 +0900

    [#2] first commit

--- README.md
+++ README.md
@@ -15,6 +15,17 @@ go get -u github.com/heaven-chp/base-client-go
  - example of use
    - socketClientConfig of socket_client/main.go
 
+## How to use grpc client
+ - install
+   - go install github.com/heaven-chp/base-client-go/grpc_client
+ - run
+   - server must be running
+     - go get -u github.com/heaven-chp/base-server-go
+     - go install github.com/heaven-chp/base-server-go/grpc_server && ./bin/grpc_server -config_file src/github.com/heaven-chp/base-server-go/config/grpc_server.config
+   - ./bin/grpc_client -config_file src/github.com/heaven-chp/base-client-go/config/grpc_client.config
+ - log
+   - ./log/grpc_client_YYYYMMDD.log
+
 ## How to use socket client
  - install
    - go install github.com/heaven-chp/base-client-go/socket_client
--- config/config_test.go
+++ config/config_test.go
@@ -5,6 +5,35 @@ import (
 	"testing"
 )
 
+func TestGrpcClient(t *testing.T) {
+	var grpcClient GrpcClient
+
+	err := json.ToStructFromFile("./grpc_client.config", &grpcClient)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if grpcClient.LogLevel != "DEBUG" {
+		t.Errorf("invalid data - LogLevel : (%s)", grpcClient.LogLevel)
+	}
+
+	if grpcClient.LogOutputPath != "./log/" {
+		t.Errorf("invalid data - LogOutputPath : (%s)", grpcClient.LogOutputPath)
+	}
+
+	if grpcClient.LogFileNamePrefix != "grpc_client" {
+		t.Errorf("invalid data - LogFileNamePrefix : (%s)", grpcClient.LogFileNamePrefix)
+	}
+
+	if grpcClient.Address != "127.0.0.1:50051" {
+		t.Errorf("invalid data - Address : (%s)", grpcClient.Address)
+	}
+
+	if grpcClient.Timeout != 3 {
+		t.Errorf("invalid data - Timeout : (%d)", grpcClient.Timeout)
+	}
+}
+
 func TestSocketClient(t *testing.T) {
 	var socketClient SocketClient
 
