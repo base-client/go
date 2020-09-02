620b07e085d0b9cf3a9d11b6a12bae1aba3711f1
chp <chp@localhost.localdomain>
2020-09-03 08:04:17 +0900

    [#2] first commit

--- config/config.go
+++ config/config.go
@@ -1,6 +1,15 @@
 // Package config provides a struct that can store json type config file
 package config
 
+type GrpcClient struct {
+	LogLevel          string `json:"log_level"`
+	LogOutputPath     string `json:"log_output_path"`
+	LogFileNamePrefix string `json:"log_file_name_prefix"`
+
+	Address string `json:"address"`
+	Timeout int    `json:"timeout"`
+}
+
 type SocketClient struct {
 	LogLevel          string `json:"log_level"`
 	LogOutputPath     string `json:"log_output_path"`
